4f8c731fd20aaec5cf9d88fb3729d71d5122dc1b
nazmulislam <nazmulislam93400@gmail.com>
2025-11-18 22:42:42 +0600

    Update equilibrium-point.cpp

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
@@ -5,12 +5,15 @@ class Solution {
         int n = arr.size();
         vector<int> prefix(n,0);
         vector<int> suffix(n,0);
+      
         prefix[0] = arr[0];
         suffix[n-1] = arr[n-1];
+      
         for(int i = 1; i<n; ++i)
            prefix[i] = prefix[i-1] + arr[i];
         for(int i = n-2; i>=0; --i)
            suffix[i] = suffix[i+1] + arr[i];
+      
         for(int i = 0; i<n; ++i) 
            if(prefix[i] == suffix[i])
               return i;
