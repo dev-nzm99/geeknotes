7938589ce3fa9c882432345bcd43fc910e23925e
nazmulislam <nazmulislam93400@gmail.com>
2025-12-05 19:55:32 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.cpp
@@ -0,0 +1,21 @@
+/*
+  Author:Nazmul Islam
+*/
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        int left = 0;
+        int right = arr.size()-1;
+        int res = arr.size();
+        while(left <= right){
+            int mid = left+(right-left)/2;
+            if(arr[mid] >= target){
+                res = mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return res;
+    }
+};
